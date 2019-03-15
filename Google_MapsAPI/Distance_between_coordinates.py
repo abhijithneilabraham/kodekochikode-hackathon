df6fe6124a4bd5ef3e62f291662640efd5534376
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-03-15 12:32:56 +0530

    Update Distance_between_coordinates.py

--- Google_MapsAPI/Distance_between_coordinates.py
+++ Google_MapsAPI/Distance_between_coordinates.py
@@ -1,9 +1,9 @@
 import googlemaps  
-gmaps = googlemaps.Client(key='********************')   #API_Key  
-origin_latitude = input("Origin Latitude :")
-origin_longitude = input("Origin Longitude :")
-destination_latitude = input("Destination Latitude :")
-destination_longitude = input("Destination Longitude :")
-distance = gmaps.distance_matrix([str(origin_latitude) + " " + str(origin_longitude)], [str(destination_latitude) + " " + str(destination_longitude)], mode='walking')['rows'][0]['elements'][0]
+gmaps = googlemaps.Client(key='********************')  #API_Key
+LatO = input("Origin Latitude :")
+LongO = input("Origin Longitude :")
+LatD = input("Destination Latitude :")
+LongD = input("Destination Longitude :")
+distance = gmaps.distance_matrix([str(LatO) + " " + str(LongO)], [str(LatD) + " " + str(LongD)], mode='walking')['rows'][0]['elements'][0]
 
 print(distance["distance"]["text"])
